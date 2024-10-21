4ff3c06b3d8f898d510dab135134bda105c8b9cf
Teemu Ruokokoski <teemu.ruokokoski@helsinki.fi>
2024-10-21 18:12:35 +0300

    Ex1-15

--- viikko1/nhl-statistics-1/src/statistics_service.py
+++ viikko1/nhl-statistics-1/src/statistics_service.py
@@ -1,10 +1,10 @@
-from player_reader import PlayerReader
+#from player_reader import PlayerReader
 
 
 
 class StatisticsService:
-    def __init__(self):
-        reader = PlayerReader()
+    def __init__(self, player_reader):
+        reader = player_reader
 
         self._players = reader.get_players()
 
