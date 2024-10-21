5b8af8645329c2947e47c7ccb37b6b41d4f1d731
Teemu Ruokokoski <teemu.ruokokoski@helsinki.fi>
2024-10-21 18:56:17 +0300

    Ex1-16

---
+++ viikko1/nhl-statistics-1/src/tests/stat_test.py
@@ -0,0 +1,41 @@
+import unittest
+from statistics_service import StatisticsService
+from player import Player
+
+class PlayerReaderStub:
+    def get_players(self):
+        return [
+            Player("Semenko", "EDM", 4, 12),
+            Player("Lemieux", "PIT", 45, 54),
+            Player("Kurri",   "EDM", 37, 53),
+            Player("Yzerman", "DET", 42, 56),
+            Player("Gretzky", "EDM", 35, 89)
+        ]
+
+class TestStatisticsService(unittest.TestCase):
+    def setUp(self):
+        # annetaan StatisticsService-luokan oliolle "stub"-luokan olio
+        self.stats = StatisticsService(
+            PlayerReaderStub()
+        )
+
+    def test_search_player(self):
+        result = self.stats.search("Kurri")
+        self.assertEqual(result.name, "Kurri")
+        self.assertEqual(result.team, "EDM")
+
+    def test_search_none(self):
+        result = self.stats.search("Rantanen")
+        self.assertEqual(result, None)
+
+    def test_team(self):
+        result = self.stats.team("PIT")
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result[0].name, "Lemieux")
+
+    def test_top(self):
+        result = self.stats.top(3)
+        self.assertEqual(result[0].name, "Gretzky")
+        self.assertEqual(result[1].name, "Lemieux")
+        self.assertEqual(result[2].name, "Yzerman")
+        
