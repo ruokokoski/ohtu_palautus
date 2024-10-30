e9241fa15217a558764fa0572c2ada4126602511
Teemu Ruokokoski <teemu.ruokokoski@helsinki.fi>
2024-10-30 16:01:55 +0200

    Ex2-10

---
+++ viikko2/nhl-reader/src/index.py
@@ -0,0 +1,28 @@
+import requests
+from player import Player
+
+def main():
+    url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
+    response = requests.get(url).json()
+
+    #print("JSON-muotoinen vastaus:")
+    #print(response)
+
+    players = []
+
+    for player_dict in response:
+        player = Player(player_dict)
+        players.append(player)
+
+    #print("Oliot:")
+
+    #for player in players:
+    #    print(player)
+
+    fin_players = [player for player in players if player.nationality == 'FIN']
+    print("Players from FIN\n")
+    for player in fin_players:
+        print(player)
+
+if __name__ == "__main__":
+    main()
