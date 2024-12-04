31989f00a1148d486219ade761fca5450ef556e2
Teemu Ruokokoski <teemu.ruokokoski@helsinki.fi>
2024-12-04 10:49:13 +0200

    Ex6-3

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,6 +1,6 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn, All, Not, HasFewerThan
+from matchers import And, HasAtLeast, PlaysIn, All, Not, HasFewerThan, Or
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players.txt"
@@ -19,16 +19,20 @@ def main():
     filtered_with_all = stats.matches(All())
     print("All:", len(filtered_with_all))
 
-    matcher = And(
-        Not(HasAtLeast(2, "goals")),
-        PlaysIn("NYR")
+    matcher = Or(
+        HasAtLeast(45, "goals"),
+        HasAtLeast(70, "assists")
     )
     for player in stats.matches(matcher):
         print(player)
     print()
     matcher = And(
-        HasFewerThan(2, "goals"),
-        PlaysIn("NYR")
+        HasAtLeast(70, "points"),
+        Or(
+            PlaysIn("NYR"),
+            PlaysIn("FLA"),
+            PlaysIn("BOS")
+        )
     )
     for player in stats.matches(matcher):
         print(player)
